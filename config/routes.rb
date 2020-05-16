2383b3b617621d992eb83d0b0091b98def8b0b14
Togo <a.kirigo.t0630@gmail.com>
2020-05-16 07:58:39 +0000

    [add]routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,33 +3,27 @@ Rails.application.routes.draw do
   devise_for :customers
   namespace :admin do
   	resources :customers,only: [:index,:show,:edit,:update]
-  	resources :products,only: [:index,:new,:create,:show,:edit,:update,] do
-  		collection do
-  	        get 'top'
-  	    end
-  	end
+  	resources :products,only: [:index,:new,:create,:show,:edit,:update,] 
+  	get 'top'=>'admin/products#top'
   	resources :genres,only: [:index,:create,:edit,:update]
-  	resources :orders,only: [:index,:show,:update]
-  	resources :order_details,only: [:update]
+  	resources :orders,only: [:index,:show,:update] do
+  	  member do
+       resource :order_details,only: [:update]
+      end
+    end
   end
+
    scope module: :customer do
   	resource :customers,only: [:show,:edit,:update] do
   		collection do
-  	        get 'quit'
-  	        patch 'out'
+  	        get 'quit' 
+  	        patch 'out' 
   	    end
   	
-        resources :products,only: [:index,:show] do
-    	     collection do
-                 get 'about'
-             end
-        end
-        resources :cart_items,only: [:index,:update,:create,] do
-              member do
-                  delete 'one_destroy'
-              end
-              collection do 
-        	      delete 'destroy'
+        resources :products,only: [:index,:show] 
+        resources :cart_items,only: [:index,:update,:create,:destroy] do
+              collection do
+                 delete '/' => 'cart_items#all_destroy'
               end
         end
         resources :orders,only: [:new,:index,:show,:create] do
@@ -41,5 +35,6 @@ Rails.application.routes.draw do
         resources :shipping_address,only: [:index,:create,:edit,:update,:destroy]
     end
    end
-  
+get  'about' => 'customer/products#about' 
+root 'customer/products#top'  
 end
