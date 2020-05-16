061a28d71cceab832dad3f76ccc7719820f4ce5a
mako <coco.jasmine18@icloud.com>
2020-05-16 10:51:57 +0000

    [add]validation

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,9 @@
 class Order < ApplicationRecord
+
+	belongs_to :customer
+
+	validates :customer_id, :address,:name, :postage,
+			  :total_price, :payment_method, :order_status,
+			  presence: true
+	validates :postal_code, length: {is: 7}
 end
