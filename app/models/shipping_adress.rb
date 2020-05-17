f1f74396018db513c7a5db53651448372e9c8ffa
mako <coco.jasmine18@icloud.com>
2020-05-17 03:59:15 +0000

    [add]validation

--- app/models/shipping_adress.rb
+++ app/models/shipping_adress.rb
@@ -3,5 +3,5 @@ class ShippingAdress < ApplicationRecord
 	belongs_to :customer
 
 	validates :customer_id, :name, :address, presence: true
-	validates :postal_code, length: {is: 7}
+	validates :postal_code, length: {is: 7}, numericality: { only_integer: true }
 end
