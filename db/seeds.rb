8d77b98c5b5aaa169931af63dea42d1f16f2d2e5
daisuke <daisukeh9404031099@gmail.com>
2020-05-17 10:07:24 +0000

    [login-complete]

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 5.times do |n|
-    User.create!(
+    Admin.create!(
       email: "test#{n + 1}@test.com",
       first_name: "姓#{n + 1}",
       last_neme: "名#{n + 1}",
