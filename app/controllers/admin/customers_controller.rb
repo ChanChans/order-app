c28714be788c83b8ded1c6f3645de73b360b8b4f
Togo <a.kirigo.t0630@gmail.com>
2020-05-18 07:51:08 +0000

    [add]配送先終わり

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,16 +1,22 @@
 class Admin::CustomersController < ApplicationController
 
+    def index
+    @customers = Customer.all
 
-
-  def index
 	end
 
 	def show
+	@customer = Customer.find(params[:id])
 	end
 
 	def edit
 	end
 
 	def update
+	end
+
+	private
+	def customer_params
+	  params.require(:customer).permit(:first_name,:email,:is_valid)
 	end
 end
