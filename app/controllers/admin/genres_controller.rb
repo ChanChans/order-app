5aaa1952c492eb60daae97fab79729876fdc22c2
daisuke <daisukeh9404031099@gmail.com>
2020-05-19 09:55:00 +0000

    [genre] finish

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -9,11 +9,11 @@ class Admin::GenresController < ApplicationController
 
   def create
     @genre = Genre.new(genre_params)
-    @genres = Genre.all
     if @genre.save
        redirect_to admin_genres_path
     else
-      render :index
+      @genres = Genre.all
+      render :index and return
     end
   end
 
@@ -23,6 +23,11 @@ class Admin::GenresController < ApplicationController
 
   def update
     @genre = Genre.find(params[:id])
+    if @genre.update(genre_params)
+       redirect_to admin_genres_path
+    else
+       render :edit and return
+    end
   end
 
   private
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_17_072219) do
+ActiveRecord::Schema.define(version: 2020_05_19_082258) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -54,7 +54,7 @@ ActiveRecord::Schema.define(version: 2020_05_17_072219) do
 
   create_table "genres", force: :cascade do |t|
     t.string "name"
-    t.boolean "is_valid", default: false, null: false
+    t.boolean "is_valid", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -94,15 +94,6 @@ ActiveRecord::Schema.define(version: 2020_05_17_072219) do
   end
 
   create_table "shipping_addresses", force: :cascade do |t|
-    t.integer "customer_id"
-    t.string "name"
-    t.string "postal_code"
-    t.string "address"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "shipping_adresses", force: :cascade do |t|
     t.integer "customer_id"
     t.string "name"
     t.string "postal_code"
