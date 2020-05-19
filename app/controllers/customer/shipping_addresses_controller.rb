5c958a19227df3801c1c530ee9c3d23b1d85ff8b
Togo <a.kirigo.t0630@gmail.com>
2020-05-19 15:56:56 +0000

    [add] その他細かい編集

--- app/controllers/customer/shipping_addresses_controller.rb
+++ app/controllers/customer/shipping_addresses_controller.rb
@@ -1,17 +1,17 @@
 class Customer::ShippingAddressesController < ApplicationController
 
-    def index
+  def index
     	@shipping_addresses = ShippingAddress.all
     	@shipping_address = ShippingAddress.new
 	end
 
 	def create
-		@shipping_address = current_customer.shipping_address.new(shipping_address_params)
-		if @shipping_address.save
-		   redirect_to customers_shipping_addresses_path
-		else
-		   @shipping_addresses = ShippingAddress.all
-		   render 'index'
+		  @shipping_address = current_customer.shipping_address.new(shipping_address_params)
+		  if @shipping_address.save
+		     redirect_to customers_shipping_addresses_path
+		  else
+		     @shipping_addresses = ShippingAddress.all
+		     render 'index'
 	    end
 	end
 
@@ -20,18 +20,18 @@ class Customer::ShippingAddressesController < ApplicationController
 	end
 
 	def update
-		@shipping_address = ShippingAddress.find(params[:id])
-		if @shipping_address.update(shipping_address_params)
-		   redirect_to customers_shipping_addresses_path
-		else
-			render "edit"
-		end
+		  @shipping_address = ShippingAddress.find(params[:id])
+		  if @shipping_address.update(shipping_address_params)
+		     redirect_to customers_shipping_addresses_path
+		  else
+			   render "edit"
+		  end
 	end
 
 	def destroy
-		@shipping_address = ShippingAddress.find(params[:id])
-		@shipping_address.destroy
-		redirect_to customers_shipping_addresses_path
+		  @shipping_address = ShippingAddress.find(params[:id])
+		  @shipping_address.destroy
+		  redirect_to customers_shipping_addresses_path
 	end
 
 	private
