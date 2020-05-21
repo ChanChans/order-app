104b7c75eb2d5265984571c0cd390b3a5a91ccb1
daisuke <daisukeh9404031099@gmail.com>
2020-05-21 10:06:32 +0000

    [finish] password

--- app/controllers/customers/registrations_controller.rb
+++ app/controllers/customers/registrations_controller.rb
@@ -15,14 +15,32 @@ class Customers::RegistrationsController < Devise::RegistrationsController
   # end
 
   # GET /resource/edit
-  # def edit
-  #   super
-  # end
+  def edit
+    super
+     # @customer = current_customer
+  end
 
   # PUT /resource
-  # def update
-  #   super
-  # end
+  def update
+    self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
+    prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
+
+    resource_updated = update_resource(resource, account_update_params)
+    yield resource if block_given?
+    if resource_updated
+      set_flash_message_for_update(resource, prev_unconfirmed_email)
+      bypass_sign_in resource, scope: resource_name if sign_in_after_change_password?
+
+      # respond_with resource, location: after_update_path_for(resource)
+      redirect_to customers_path
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      # respond_with resource
+      redirect_to customers_path
+    end
+    # super
+  end
 
   # DELETE /resource
   # def destroy
@@ -47,8 +65,20 @@ class Customers::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
+  #   devise_parameter_sanitizer.permit(:account_update, keys: [:password, :password_confirmation])
+  # end
+
+  # def configure_account_update_params
   #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
   # end
+
+  def update_resource(resource, params)
+    resource.update_with_password(params)
+  end
+
+  def after_update_path_for(resource)
+    customers_path(resource)
+  end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
