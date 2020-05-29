9fe73437ab8fe29fcdb6f748550ecb50fcac92e6
atsuki <3flower.atu87@gmail.com>
2020-05-29 03:57:52 +0000

    [Fix]seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,19 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+20.times do |n|
+    Customer.create!(
+      email: "test#{n + 1}@test.com",
+      first_name: "名#{n + 1}",
+      last_name: "姓#{n + 1}",
+      kana_first_name: "セイ",
+      kana_last_name: "メイ",
+      password: "123456",
+      postal_code: "1234567",
+      residence: "東京都渋谷区神南1丁目19-11 パークウェースクエア2 4階#{n + 1}",
+      phone_number: "09012345678"
+    )
+end
 
   Admin.create!(
     email: ENV['admin_mail'],
@@ -248,18 +261,4 @@ end
     quantity: n + 3,
     subprice: "#{1000 + (n * 1000)}"
   )
-end
-
-20.times do |n|
-    Customer.create!(
-      email: "test#{n + 1}@test.com",
-      first_name: "名#{n + 1}",
-      last_name: "姓#{n + 1}",
-      kana_first_name: "セイ",
-      kana_last_name: "メイ",
-      password: "123456",
-      postal_code: "1234567",
-      residence: "東京都渋谷区神南1丁目19-11 パークウェースクエア2 4階#{n + 1}",
-      phone_number: "09012345678"
-    )
 end
